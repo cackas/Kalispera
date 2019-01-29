41cb9a9106fe4b22b8108895fdc9f7c54618222f
cackas <vannigga@yandex.ru>
2019-01-29 20:39:49 +0300

    comm5

--- gulp/client/js/common.js
+++ gulp/client/js/common.js
@@ -12,4 +12,23 @@ $(document).ready(function() {
       });
       return false;
     });
+    //slider home
+    $('.education__slider').slick({
+        vertical: true,
+        verticalSwiping: true,
+        arrows: false
+    });
+    $('.top-arrow').click(function(){
+        $('.education__slider').slick('slickPrev');
+    })
+    $('.bott-arrow').click(function(){
+        $('.education__slider').slick('slickNext');
+    })
+    $('.loan__slider .countries').slick({
+        rows: 2,
+        slidesPerRow: 4,
+        infinite: false,
+        dots: true,
+        arrows: false
+    });
 })
